8d6ec16dc3c6dfc28dc87d16f7b928719430d9fc
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2023-08-18 19:56:25 +0530

    3rd commit
    getting the length of the string

--- hello.js
+++ hello.js
@@ -3,3 +3,7 @@ console.log("Hello Buddy !")
 
 //another line of code added after the initial commit
 console.log("Adding another line of code")
+
+//another two lines added (getting the length of the string)
+let name = "Ravoof";
+console.log(name.length)
